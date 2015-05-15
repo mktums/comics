66bce24eb17cad3812259b26eacaa0a77e39c77c
Mike Tums <mktums@gmail.com>
2015-05-15 13:37:48 +0300

    'Fixed' QuestionableContent spider. As of now, archive page contains a lot of broken links and screwed titles, so we don't use them.

--- comics/spiders/qc.py
+++ comics/spiders/qc.py
@@ -11,10 +11,13 @@ from comics.utils import remove_disallowed_filename_chars, download
 class YAFGCSpider(CrawlSpider):
     PATH = 'Archive/QuestionableContent'
     name = 'qc'
-    start_urls = ['http://questionablecontent.net/archive.php', ]
+    start_urls = [
+        # 'http://questionablecontent.net/archive.php', # As of now it contains a LOT of errors
+        'http://www.questionablecontent.net'
+    ]
     allowed_domains = ['questionablecontent.net', ]
     rules = (
-        Rule(SgmlLinkExtractor(allow=(r'view\.php\?comic=\d+$',)), callback='parse_item'),
+        Rule(SgmlLinkExtractor(allow=(r'view\.php\?comic=\d+$',)), follow=True, callback='parse_item'),
     )
 
     def parse_start_url(self, response):
@@ -23,13 +26,14 @@ class YAFGCSpider(CrawlSpider):
 
     def parse_item(self, response):
         sel = Selector(response)
-        no, image_name = response.meta.get('link_text').split(': ', 1)
-        no = no.replace('Comic ', '')
         image_url = sel.xpath('//img[@id="strip"]/@src').extract()[0]
         base_url = get_base_url(response)
         image_url = urljoin(base_url, image_url)
-        _, ext = image_url.split('/')[-1].rsplit('.', 1)
-        image_name = u'{}.{}.{}'.format(no, image_name, ext)
+        no, ext = image_url.split('/')[-1].rsplit('.', 1)
+        # no, image_name = response.meta.get('link_text').split(': ', 1)
+        # no = no.replace('Comic ', '')
+        # image_name = u'{}.{}.{}'.format(no, image_name, ext)
+        image_name = u'{}.{}'.format(no, ext)
         path = self.PATH + '/' + remove_disallowed_filename_chars(image_name)
         if not exists(path):
             return download(image_url, path, response.url, self.cookie)
