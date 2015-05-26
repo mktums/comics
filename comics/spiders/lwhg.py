60cf4f814ee26c4bb4c434254c17736992158ad1
Mike Tums <mktums@gmail.com>
2015-05-26 03:53:44 +0300

    Ok. So Jago messed up with dates in his WP installation, no meaning in title, etc…

--- comics/spiders/lwhg.py
+++ comics/spiders/lwhg.py
@@ -2,8 +2,8 @@
 from os.path import exists
 
 from urlparse import urljoin
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.spiders import CrawlSpider
+from scrapy.http import Request
 from scrapy.selector import Selector
 from scrapy.utils.response import get_base_url
 
@@ -15,15 +15,10 @@ class LWHGSpider(CrawlSpider):
     name = 'lwhg'
     start_urls = ['http://www.jagodibuja.com/webcomic-living-with-hipstergirl-and-gamergirl-english/', ]
     allowed_domains = ['jagodibuja.com', ]
-    rules = (
-        Rule(SgmlLinkExtractor(
-            allow=(r'/webcomic-living-with-hipstergirl-and-gamergirl-english/.+/$',),
-            deny=(r'/feed/$',),
-        ), callback='parse_item', follow=True),
-    )
 
     def parse_start_url(self, response):
-        image_url = Selector(response).xpath('//div[@class="entry-content"]/p/a/@href').extract()[0]
+        sel = Selector(response)
+        image_url = sel.xpath('//div[@class="entry-content"]/p/a/@href').extract()[0]
         base_url = get_base_url(response)
         image_url = urljoin(base_url, image_url)
         _, ext = image_url.split('/')[-1].split('.')
@@ -31,12 +26,15 @@ class LWHGSpider(CrawlSpider):
         path = self.PATH + '/' + remove_disallowed_filename_chars(image_name)
         if not exists(path):
             download(image_url, path)
-        return super(LWHGSpider, self).parse_start_url(response)
+
+        images = sel.xpath('//figure[@class="gallery-item"]//a/@href').extract()
+        for i, c in enumerate(images):
+            yield Request(c, meta={'index': i+1}, callback=self.parse_item)
 
     def parse_item(self, response):
         sel = Selector(response)
         image_url = sel.xpath('//span[@class="full-size-link"]/a/@href').extract()[0]
-        no = filter(unicode.isdigit, sel.xpath('//h1/text()').extract()[0])
+        no = response.meta.get('index')
         base_url = get_base_url(response)
         image_url = urljoin(base_url, image_url)
         _, ext = image_url.split('/')[-1].split('.')
